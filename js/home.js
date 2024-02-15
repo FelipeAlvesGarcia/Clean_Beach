7ba3a3d50ff3a8741c2843605bf63297a3c71e27
FelipeAlvesGarcia <134567205+FelipeAlvesGarcia@users.noreply.github.com>
2024-02-15 15:12:33 -0300

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,47 @@
+//VW
+let vh = window.innerHeight || document.documentElement.clientHeight
+let vwtamanho = window.innerWidth || document.documentElement.clientWidth
+let vw = vwtamanho / 100;
+//
+
+//Tartaruga(fonte)
+const tarfonte = document.getElementById("tarfonte");
+const tar = document.getElementById("tar");
+
+tar.addEventListener("mouseover", function (){
+    tarfonte.style.display = "block";
+});
+tarfonte.addEventListener("mouseover", function (){
+    tarfonte.style.display = "block";
+});
+tar.addEventListener("mouseout", function (){
+    tarfonte.style.display = "none";
+});
+//
+
+//Carrossel
+const carro = document.getElementById("carro");
+const itens = document.getElementById("itens");
+
+var index = 0;
+var quantia = 2;
+var tamanho;
+function carativo (){
+    index++;
+    if (index >= 2)
+    {
+        index=0;
+    }
+    tamanho = 50 * index;
+    itens.style.transform =  `translatex(${-tamanho}%)`;
+    console.log(vwtamanho);
+}
+
+var intervalo;
+function car () {
+    if (vwtamanho <= 900)
+    {
+        intervalo = setInterval(carativo, 6000);
+    }
+}
+//
